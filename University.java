205ff20a6de1c6432b80fbee4262ba99bbdd76c5
CEHARVEY <ceharvey@viewlincsci23.ad.csbsju.edu>
2017-02-22 12:59:40 -0600

    Caitlin: University class structure w/ instance variables, no methods yet

---
+++ University.java
@@ -0,0 +1,15 @@
+/*
+ * File: University.java
+ */
+
+import java.util.*;
+
+public class University {
+  
+  private String schoolName, state, location, control;
+  private int numOfEnrolled, totNumOfApplicant, academicsScale, socialScale, qualOfLifeScale;
+  private Double femaleRatio, satVerbal, satMath, expenses, perAdmitted, perEnrolled;
+  private ArrayList<String> emphases;
+  
+  
+}
