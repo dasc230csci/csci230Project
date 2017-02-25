e2f1e9c2c60c4e6e0ef0387e4d0c171aaba2eee1
ceharvey <ceharvey@viewlincsci01.ad.csbsju.edu>
2017-02-25 15:48:22 -0600

    Caitlin: AdminUI initial creation

---
+++ AdminUI.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+/**
+ * File: AdminUI.java
+ */
+
+/**
+ * Class to represent the Admin UI.
+ * 
+ * @author Caitlin Harvey, Anh Tran, Sam Halloran, Daniel Song
+ */
+public class AdminUI {
+
+	/**
+	 * Admin UI constructor.
+	 */
+	public AdminUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * View a list of all users.
+	 * 
+	 * @return ArrayList of users
+	 */
+	public ArrayList viewAllUser() {
+		return null;
+	}
+	
+	/**
+	 * Deactivate a specific user.
+	 * 
+	 * @return boolean
+	 */
+	public boolean deactivateUser() {
+		return true;
+	}
+	
+	/**
+	 * Add a university.
+	 * 
+	 * @return boolean
+	 */
+	public boolean addUniversity() {
+		return true;
+	}
+	
+	/**
+	 * Edit a university.
+	 * 
+	 * @return boolean
+	 */
+	public boolean editUniversity() {
+		return true;
+	}
+	
+	/**
+	 * Create a new user.
+	 * 
+	 * @return boolean
+	 */
+	public boolean createUser() {
+		return true;
+	}
+	
+	/**
+	 * Edit an existing user.
+	 * 
+	 * @return boolean
+	 */
+	public boolean editUserProfile() {
+		return true;
+	}
+	
+	/**
+	 * Resets a user's information.
+	 * 
+	 * @return ArrayList
+	 */
+	public ArrayList resetUserInfo() {
+		return null;
+	}
+	
+	/**
+	 * View a specific user.
+	 * 
+	 * @return User
+	 */
+	//change return to User when class is created
+	public void viewSpecificUser() {
+		//return null;
+	}
+	
+	/**
+	 * Confirmation message to confirm a reset.
+	 * 
+	 * @return boolean
+	 */
+	public boolean confirmReset() {
+		return true;
+	}
+}
+
