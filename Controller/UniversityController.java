969b9f2cf8d568841b2874a245125bfd6583a926
ABTRAN <abtran@nx.ad.csbsju.edu>
2017-03-11 20:28:27 -0600

    Anh create new UserFuncContrller

--- Controller/UniversityController.java
+++ Controller/UniversityController.java
@@ -16,53 +16,58 @@ import Entity.University;
 
 public class UniversityController {
 
-	/**
-	 * University to edit, add or control university
-	 */
-	private University university;
-	
-	/**
-	 * DBController to access to database to edit, or add new university
-	 */
-	private DBController dbController;
+ /**
+  * University to edit, add or control university
+  */
+ private University university;
+ 
+ /**
+  * DBController to access to database to edit, or add new university
+  */
+ private DBController dbController;
 
-	/**
-	 * A constructor to create a new university controller that has a specific
-	 * university
-	 * @param university the university
-	 */
-	public UniversityController(University university) {
-		this.university = university;
-	}
+ /**
+  * A default constructor for the controller class
+  */
+ public UniversityController(){
+ }
+ /**
+  * A constructor to create a new university controller that has a specific
+  * university
+  * @param university the university
+  */
+ public UniversityController(University university) {
+  this.university = university;
+ }
 
-	/**
-	 * A method that create a new university based on the array of input
-	 * 
-	 * @param schoolInfo the information to create a new university
-	 * @return an university
-	 */
-	public University createUniversity(ArrayList<String> schoolInfo) {
-		return null;
-	}
+ /**
+  * A method that create a new university based on the array of input
+  * 
+  * @param schoolInfo the information to create a new university
+  * @return an university
+  */
+ public University createUniversity(ArrayList<String> schoolInfo) {
+  return null;
+ }
 
-	/**
-	 * A method that get the details of the University
-	 * 
-	 * @param schoolName the name of the University that we need the information
-	 * @return University the university with the complete information
-	 */
-	public University getUniversityInDetailed(String schoolName) {
-		return null;
-	}
+ /**
+  * A method that get the details of the University
+  * 
+  * @param schoolName the name of the University that we need the information
+  * @return University the university with the complete information
+  */
+ public University getUniversityInDetailed(String schoolName) {
+  return null;
+ }
 
-	/**
-	 * A method that set the details of the university
-	 * 
-	 * @param editInfo
-	 *            the array that contains the information that need to be change
-	 * @return boolean if the changes are made or not
-	 */
-	public boolean editUniversityInDetailed(ArrayList<String> editInfo) {
-		return false;
-	}
+ /**
+  * A method that set the details of the university
+  * 
+  * @param editInfo
+  *            the array that contains the information that need to be change
+  * @return boolean if the changes are made or not
+  */
+ public boolean editUniversityInDetailed(ArrayList<String> editInfo) {
+  return false;
+ }
 }
---
+++ Controller/UserFuncController.java~
@@ -0,0 +1,111 @@
+/**
+ * File: UserFuncController.java
+ */
+
+package Controller;
+
+import java.util.ArrayList;
+
+import Entity.Account;
+import Entity.University;
+
+/**
+ * Controller class with functions for the User.
+ * 
+ * @author  Daniel M. Song, Anh B. Tran, Caitlin E. Harvey, Samuel J. Halloran
+ * @version Feburary 27, 2017
+ */
+public class UserFuncController {
+	
+	/**
+	 * University controller to control university
+	 */
+	private UniversityController universityController;
+	
+	/**
+	 * AccountController to edit profile
+	 */
+	private AccountController accountController;
+	
+	/**
+	 * SearchController to search university
+	 */
+	private SearchController searchController;
+	
+	/**
+	 * Account instance variable
+	 */
+	private Account account;
+	
+	/**
+	 * Creates the Controller.
+	 */
+	public UserFuncController() {
+	}
+	
+	/**
+	 * Saves a school to User account.
+	 * @param schoolName - Name of school to save
+	 * @returns true if the school is successfully saved 
+	 */
+	public boolean saveSchool(String schoolName) {
+		return false;	
+	}
+	
+	/**
+	 * Allows user to edit individual profile.
+	 * 
+	 * @param firstname first name of the user
+	 * @param lastname last name of the user
+	 * @param password password of the user
+	 * @return true if successfully edit profile
+	 */
+	public boolean editProfile(String firstName, String lastName, String password){
+		return false;
+	}
+
+	/**
+	 * Allows user to view the list of saved school
+	 * @param ArrayList<String> list of saved school of user
+	 */
+	public ArrayList<String> viewSavedSchool(){
+		return null;
+	}
+	
+	/**
+	 * Removes a saved University from user account.
+	 * 
+	 * @param schoolName name of the school to remove
+	 * @return true if the school is successfully removed;
+	 */
+	public boolean removeSavedUniversity(String schoolName){
+		return true;
+	}
+	
+	/**
+	 * Resets user information.
+	 * 
+	 * @return ArrayList<String> information of the user
+	 */
+	public ArrayList<String> resetUserInfo() {
+		return null;
+	}
+	
+	/**
+	 * Gets the university is greater detail.
+	 * 
+	 * @param schoolName name of the school to view in detail
+	 * @return University
+	 */
+	public University getUniversityInDetail(String schoolName) {
+		return null;
+	}
+	/**
+	 * A method that get the saved school for student
+	 * 
+	 * @return the array list that has the name of all the saved school
+	 */
+	public ArrayList<String> getSavedSchool() {
+		return null;
+	}
+}
