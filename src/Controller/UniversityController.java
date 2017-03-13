af8eaae3e01e4a7cfbe294ec082971c2d0cedf2b
SJHALLORAN <sjhalloran@viewlincsci32.ad.csbsju.edu>
2017-03-13 15:00:20 -0500

    tset

---
+++ src/Controller/UniversityController.java
@@ -0,0 +1,73 @@
+
+/**
+ * File: UniveristyController.java
+ */
+package Controller;
+import java.util.ArrayList;
+
+import Entity.University;
+
+/**
+ * A Class representing a University Controller and all its functions.
+ * 
+ * @author  Daniel M. Song, Anh B. Tran, Caitlin E. Harvey, Samuel J. Halloran
+ * @version Feburary 27, 2017
+ */
+
+public class UniversityController {
+
+ /**
+  * University to edit, add or control university
+  */
+ private University university;
+ 
+ /**
+  * DBController to access to database to edit, or add new university
+  */
+ private DBController dbController;
+
+ /**
+  * A default constructor for the controller class
+  */
+ public UniversityController(){
+ }
+ /**
+  * A constructor to create a new university controller that has a specific
+  * university
+  * @param university the university
+  */
+ public UniversityController(University university) {
+  this.university = university;
+ }
+
+ /**
+  * A method that create a new university based on the array of input
+  * 
+  * @param schoolInfo the information to create a new university
+  * @return an university
+  */
+ public University createUniversity(ArrayList<String> schoolInfo) {
+  return null;
+ }
+
+ /**
+  * A method that get the details of the University
+  * 
+  * @param schoolName the name of the University that we need the information
+  * @return University the university with the complete information
+  */
+ public University getUniversityInDetailed(String schoolName) {
+  return null;
+ }
+
+ /**
+  * A method that set the details of the university
+  * 
+  * @param editInfo
+  *            the array that contains the information that need to be change
+  * @return boolean if the changes are made or not
+  */
+ public boolean editUniversityInDetailed(ArrayList<String> editInfo) {
+  return false;
+ }
+}
